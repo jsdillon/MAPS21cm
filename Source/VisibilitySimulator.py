b8063fe30b5dd0e67b06890cb0466485a42239a2
Josh Dillon <jsdiillon@mit.edu>
2015-05-17 08:02:14 +0200

    New GSM class

--- Source/VisibilitySimulator.py
+++ Source/VisibilitySimulator.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 import ephem
 import Source.MapmakerHelper as mmh
+from Source.GlobalSkyModel import GlobalSkyModel as gsm
 
 
 def VisibilitySimulator(s,g,PBs):
@@ -15,13 +16,9 @@ def VisibilitySimulator(s,g,PBs):
     
     #TODO: this ignores polarization and differing primary beams
     if s.simulateVisibilitiesWithGSM:
-        #Compute GSM from 3 principal components appropriately weighted and then interpolate onto rotated equatorial coordinates
-        GSMComponents = np.asarray([hp.read_map(s.GSMlocation + "gsm" + str(i+1) + ".fits" + str(s.GSMNSIDE),verbose=False,) for i in range(3)])
-        components = np.loadtxt(s.GSMlocation + "components.dat")
-        temp = 10**(interp1d(np.log10(components[:,0]), np.log10(components[:,1]), kind='cubic')(np.log10(s.freq))) #cubic spline in log(T)
-        weights = np.asarray([interp1d(components[:,0], components[:,i+2], kind='linear')(s.freq) for i in range(3)]) #linear interpolation for weights
-        GSM = temp*np.dot(weights,GSMComponents)
-        interpoltedGSMRotated = hp.get_interp_val(GSM,-g.galCoords.b.radian+np.pi/2, np.asarray(g.galCoords.l.radian))
+        GSM = gsm(s.freq, s.GSMlocation, s.GSMNSIDE)
+        #interpolate onto rotated equatorial coordinates
+        interpoltedGSMRotated = hp.get_interp_val(GSM.hpMap,-g.galCoords.b.radian+np.pi/2, np.asarray(g.galCoords.l.radian))
         hp.mollview(np.log10(interpoltedGSMRotated), title="GSM in Rotated Equatorial Coordinates")
 
 
@@ -36,7 +33,7 @@ def VisibilitySimulator(s,g,PBs):
             primaryBeam = hp.get_interp_val(PBs.beamSquared("X","x",s.pointings[0]), np.pi/2-pixelAlts, pixelAzs)
             for b in range(len(s.baselines)):
                 exponent = np.exp(-2j*np.pi*np.dot(rHatVectors,s.baselines[b]))
-                visibilities[t,b] += np.sum(GSM * primaryBeam * exponent) * 4*np.pi / len(GSM)
+                visibilities[t,b] += np.sum(GSM.hpMap * primaryBeam * exponent) * 4*np.pi / len(GSM.hpMap)
                 
 #    if s.simulateVisibilitiesWithPointSources:
         #Load PS catalog
