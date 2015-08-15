Started working on unit testing, fixed visibility simulation bug, still hunting normalization bug

Mapmaker.py
@@ -11,53 +11,112 @@ from Source import Geometry
 from Source.PointSourceCatalog import PointSourceCatalog
 from Source import MatricesForMapmaking as MapMats
 import scipy.constants as const
-plt.close("all")
 
-freq = 150
-#TODO: the frequency should be passed as a command line parameters
-
-#Load in everything we need, figure out which LSTs to work with
-scriptDirectory = os.path.dirname(os.path.abspath(__file__))
-s = Specifications(scriptDirectory, "/configuration.txt",freq)
-times = Geometry.Times(s)
-times.CutOutUnusedLSTsAndGroupIntoSnapshots(s)
-coords = Geometry.Coordinates(s)
-PBs = PrimaryBeams(s)
-ps = PointSourceCatalog(s,PBs,times)
+from Source.GlobalSkyModel import GlobalSkyModel
+plt.close('all')
 
 
-#Simulate or load visibilities
-if s.simulateVisibilitiesWithGSM or s.simulateVisibilitiesWithPointSources:
-    visibilities = VisibilitySimulator(s,PBs,ps,times)
-else:
-    print "Visibility loading functions are not done."    
-    #visibilties = LoadVisibilities(s)
-visibilities *= s.convertJyToKFactor
-
-#Prepare visibilities
-Geometry.rephaseVisibilitiesToSnapshotCenter(s,visibilities,times)
-MapMats.inverseCovarianceWeightVisibilities(s,visibilities)
-
-#Perform mapmaking and calculate PSFs
-coaddedMap = np.zeros(coords.nFacetPixels)
-PSF = np.zeros((coords.nFacetPixels,coords.nExtendedPixels))
-pointSourcePSF = np.zeros((coords.nFacetPixels, ps.nSources))
-for snapshot in times.snapshots:    
-    NinvTimesy = MapMats.calculateNinvTimesy(visibilities, snapshot)
-    Ninv = MapMats.calculateNInv(s,snapshot)
-    KAtranspose = MapMats.calculateKAtranspose(s,snapshot,coords,PBs)    
-    coaddedMap += 2 * np.real(np.dot(KAtranspose[coords.mapIndexLocationsInExtendedIndexList,:], NinvTimesy))
-    PSF += 2 * np.real(np.dot(KAtranspose[coords.mapIndexLocationsInExtendedIndexList,:], np.dot(np.diag(Ninv), KAtranspose.conj().transpose())))    
-    if s.PSFforPointSources and ps.nSources > 0:
-        pointSourceAmatrix = MapMats.calculatePSAmatrix(s,snapshot,ps,PBs)
-        pointSourcePSF += 2 * np.real(np.dot(KAtranspose[coords.mapIndexLocationsInExtendedIndexList,:], np.dot(np.diag(Ninv), pointSourceAmatrix)))
-        
-#Renormalize maps and PSFs and save results
-Dmatrix = np.diag(np.diag(PSF[:,coords.mapIndexLocationsInExtendedIndexList])**(-1))
-PSF = np.dot(Dmatrix,PSF)
-coaddedMap = np.dot(Dmatrix,coaddedMap)
-pointSourcePSF = np.dot(Dmatrix,pointSourcePSF)
-mapNoiseCovariance = np.dot(PSF[:,coords.mapIndexLocationsInExtendedIndexList],np.transpose(Dmatrix))
-
-MapMats.saveAllResults(s,coords,times,ps,Dmatrix,PSF,coaddedMap,pointSourcePSF,mapNoiseCovariance)
+def Mapmaker(freq = 150, configFile = "configuration.txt", overridePSFExtension = -1):
+    """This function makes maps from visibilities and also calculates the associated map statistics. 
     
+    Saves the results to binary (as pickles or numpy arryas) and returns the folder where they are located"""
+    
+    #Load in everything we need, figure out which LSTs to work with
+    scriptDirectory = os.path.dirname(os.path.abspath(__file__))
+    s = Specifications(scriptDirectory, "/" + configFile,freq)
+    if overridePSFExtension > 0:
+        s.PSFextensionBeyondFacetFactor = overridePSFExtension    
+    
+    times = Geometry.Times(s)
+    times.CutOutUnusedLSTsAndGroupIntoSnapshots(s)
+    coords = Geometry.Coordinates(s)
+    PBs = PrimaryBeams(s)
+    ps = PointSourceCatalog(s,PBs,times)
+    
+    
+    #Simulate or load visibilities
+    if s.simulateVisibilitiesWithGSM or s.simulateVisibilitiesWithPointSources:
+        visibilities = VisibilitySimulator(s,PBs,ps,times,coords)
+    else:
+        print "Visibility loading functions are not done."    
+        #visibilties = LoadVisibilities(s)
+    visibilities *= s.convertJyToKFactor
+    
+    #Prepare visibilities
+    Geometry.rephaseVisibilitiesToSnapshotCenter(s,visibilities,times)
+    MapMats.inverseCovarianceWeightVisibilities(s,visibilities)
+
+    
+    #Perform mapmaking and calculate PSFs
+    coaddedMap = np.zeros(coords.nFacetPixels)
+    PSF = np.zeros((coords.nFacetPixels,coords.nExtendedPixels))
+    pointSourcePSF = np.zeros((coords.nFacetPixels, ps.nSources))
+    for snapshot in times.snapshots:    
+        NinvTimesy = MapMats.calculateNinvTimesy(visibilities, snapshot)
+        Ninv = MapMats.calculateNInv(s,snapshot)
+        KAtranspose = MapMats.calculateKAtranspose(s,snapshot,coords,PBs)    
+        coaddedMap += 2 * np.real(np.dot(KAtranspose[coords.mapIndexLocationsInExtendedIndexList,:], NinvTimesy))
+        PSF += 2 * np.real(np.dot(KAtranspose[coords.mapIndexLocationsInExtendedIndexList,:], np.dot(np.diag(Ninv), KAtranspose.conj().transpose())))    
+        if s.PSFforPointSources and ps.nSources > 0:
+            pointSourceAmatrix = MapMats.calculatePSAmatrix(s,snapshot,ps,PBs)
+            pointSourcePSF += 2 * np.real(np.dot(KAtranspose[coords.mapIndexLocationsInExtendedIndexList,:], np.dot(np.diag(Ninv), pointSourceAmatrix)))
+            
+    #Renormalize maps and PSFs and save results
+    Dmatrix = np.diag(np.diag(PSF[:,coords.mapIndexLocationsInExtendedIndexList])**(-1))
+    #Dmatrix = np.diag(np.ones(len(coords.mapIndices)))
+    #print "warning: Dmatrix set to Identity"
+    PSF = np.dot(Dmatrix,PSF)
+    coaddedMap = np.dot(Dmatrix,coaddedMap)
+    mapNoiseCovariance = np.dot(PSF[:,coords.mapIndexLocationsInExtendedIndexList],np.transpose(Dmatrix))
+    if s.PSFforPointSources and ps.nSources > 0:
+        pointSourcePSF = np.dot(Dmatrix,pointSourcePSF)
+    
+    
+
+
+    s.GSMNSIDE = s.mapNSIDE
+    coordsGSM = Geometry.Coordinates(s,True)
+    GSM = GlobalSkyModel(s.freq, s.GSMlocation, s.GSMNSIDE)
+    interpoltedGSMRotated = hp.get_interp_val(GSM.hpMap,-coordsGSM.galCoords.b.radian+np.pi/2, np.asarray(coordsGSM.galCoords.l.radian))
+    interpoltedGSMRotated = np.zeros(len(interpoltedGSMRotated))
+    testSourceIndex = hp.query_disc(coords.NSIDE, hp.ang2vec(np.pi/2, 0), s.facetSize/10 * 2*np.pi/360.0)[0]
+    interpoltedGSMRotated[testSourceIndex] = 1
+    convolvedGSM = np.dot(PSF,interpoltedGSMRotated[coords.extendedIndices])
+    plt.figure()    
+    hp.mollview(interpoltedGSMRotated, title="interpoltedGSMRotated")
+    
+    def plotFacet(s,coords,facetMap,plotTitle):
+        mapToPlot = np.zeros(coords.mapPixels)
+        mapToPlot[coords.mapIndices] = facetMap    
+        hp.mollview(mapToPlot, title=plotTitle)
+        plt.axis(np.asarray([-1,1,-1,1]) * s.facetSize/50)
+    plt.figure()
+    plt.plot(visibilities)
+    plotFacet(s,coords,interpoltedGSMRotated[coords.mapIndices],"GSM")    
+
+    mapToPlot = np.zeros(coords.mapPixels)
+    mapToPlot[coords.mapIndices] = PSF[:,np.nonzero(coords.extendedIndices == testSourceIndex)[0][0]]    
+    hp.mollview(mapToPlot, title="PSF")
+    plt.axis(np.asarray([-1,1,-1,1]) * s.facetSize/50)
+
+
+    plotFacet(s,coords,convolvedGSM,"Convolved GSM")
+    plotFacet(s,coords,coaddedMap,"coaddedMap")
+    plotFacet(s,coords,coaddedMap/convolvedGSM,'map/convolved ratio')
+      
+    plt.plot(np.diag(Dmatrix))
+    plt.show()
+
+
+    
+    
+    
+    
+    
+    MapMats.saveAllResults(s,coords,times,ps,Dmatrix,PSF,coaddedMap,pointSourcePSF,mapNoiseCovariance)
+    return s.resultsFolder
+    
+    
+
+if __name__ == "__main__":
+    Mapmaker()

Source/Geometry.py
@@ -89,7 +89,7 @@ class Times:
         LSTindicesToUse = np.nonzero(self.useThisLST)[0]
         closeEnoughLSTs = len(LSTindicesToUse)
         consecutiveSegments =  np.split(LSTindicesToUse, np.where(np.diff(LSTindicesToUse) != 1)[0]+1)
-        LSTindicesToUse = np.asarray([segment[0:np.floor(len(segment)/s.integrationsPerSnapshot)*s.integrationsPerSnapshot] for segment in consecutiveSegments]).flatten()  
+        LSTindicesToUse = np.asarray([segment[index] for segment in consecutiveSegments for index in range(int(np.floor(len(segment)/s.integrationsPerSnapshot)*s.integrationsPerSnapshot))])
         
         #Delete unnecessary data
         self.useThisLST = np.zeros(len(self.LSTs))

Source/VisibilitySimulator.py
@@ -10,7 +10,7 @@ import Geometry
 from GlobalSkyModel import GlobalSkyModel
 
 
-def VisibilitySimulator(s,PBs,ps,times):
+def VisibilitySimulator(s,PBs,ps,times,coords):
     print "Now simulating visibilities (assuming XX beams only)..."
     coordsGSM = Geometry.Coordinates(s,True)
     visibilities = np.zeros([len(times.LSTs),len(s.baselines)],dtype=complex)
@@ -18,9 +18,18 @@ def VisibilitySimulator(s,PBs,ps,times):
     #TODO: this ignores polarization and differing primary beams
     if s.simulateVisibilitiesWithGSM:
         GSM = GlobalSkyModel(s.freq, s.GSMlocation, s.GSMNSIDE)
-        #interpolate onto rotated equatorial coordinates
-        interpoltedGSMRotated = hp.get_interp_val(GSM.hpMap,-coordsGSM.galCoords.b.radian+np.pi/2, np.asarray(coordsGSM.galCoords.l.radian))
-        hp.mollview(np.log10(interpoltedGSMRotated), title="GSM in Rotated Equatorial Coordinates")
+        interpoltedGSMRotated = hp.get_interp_val(GSM.hpMap,-coordsGSM.galCoords.b.radian+np.pi/2, np.asarray(coordsGSM.galCoords.l.radian))        
+        
+        
+        interpoltedGSMRotated = np.zeros(len(interpoltedGSMRotated))
+        testSourceIndex = hp.query_disc(coords.NSIDE, hp.ang2vec(np.pi/2, 0), s.facetSize/10 * 2*np.pi/360.0)[0]
+        facetTest = np.zeros(len(interpoltedGSMRotated))
+        facetTest[coords.mapIndices] = 1
+        facetTest[testSourceIndex] = 2
+        hp.mollview(facetTest, title="facetTest")
+        interpoltedGSMRotated[testSourceIndex] = 1
+        #hp.mollview(np.log10(interpoltedGSMRotated), title="GSM in Rotated Equatorial Coordinates")
+        
         
         #loop over times and baselines to calculate visibilities
         for t in range(len(times.LSTs)):
@@ -28,8 +37,8 @@ def VisibilitySimulator(s,PBs,ps,times):
             rHatVectors = Geometry.convertAltAzToCartesian(pixelAlts,pixelAzs)
             primaryBeam = hp.get_interp_val(PBs.beamSquared("X","x",s.pointings[t]), np.pi/2-pixelAlts, pixelAzs)
             for b in range(len(s.baselines)):
-                exponent = np.exp(-2j*np.pi*np.dot(rHatVectors,s.baselines[b]))
-                visibilities[t,b] += np.sum(GSM.hpMap * primaryBeam * exponent) * 4*np.pi / len(GSM.hpMap) / s.convertJyToKFactor
+                exponent = np.exp(-1j*s.k*np.dot(rHatVectors,s.baselines[b]))
+                visibilities[t,b] += np.sum(interpoltedGSMRotated * primaryBeam * exponent) * 4*np.pi / len(GSM.hpMap) / s.convertJyToKFactor
 
     if s.simulateVisibilitiesWithPointSources and ps.nSources > 0:
         for t in range(len(times.LSTs)):
@@ -37,7 +46,7 @@ def VisibilitySimulator(s,PBs,ps,times):
             rHatVectors = Geometry.convertAltAzToCartesian(psAlts,psAzs)
             primaryBeam = hp.get_interp_val(PBs.beamSquared("X","x",s.pointings[t]), np.pi/2-psAlts, psAzs)
             for b in range(len(s.baselines)):
-                exponent = np.exp(-2j*np.pi*np.dot(rHatVectors,s.baselines[b]))
+                exponent = np.exp(-1j*s.k*np.dot(rHatVectors,s.baselines[b]))
                 visibilities[t,b] += np.sum(ps.scaledFluxes * primaryBeam * exponent) 
 				
     return visibilities