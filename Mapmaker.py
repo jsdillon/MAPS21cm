Fixed normalization error, no on to more unit testing when I get back from vacation

Mapmaker.py
@@ -32,7 +32,7 @@ def Mapmaker(freq = 150, configFile = "configuration.txt", overridePSFExtension 
     coords = Geometry.Coordinates(s)
     PBs = PrimaryBeams(s)
     ps = PointSourceCatalog(s,PBs,times)
-    
+    print s.convertJyToKFactor
     
     #Simulate or load visibilities
     if s.simulateVisibilitiesWithGSM or s.simulateVisibilitiesWithPointSources:
@@ -43,6 +43,7 @@ def Mapmaker(freq = 150, configFile = "configuration.txt", overridePSFExtension 
     visibilities *= s.convertJyToKFactor
     
     #Prepare visibilities
+    visibilities /= s.convertJyToKFactor #converts to temperature units
     Geometry.rephaseVisibilitiesToSnapshotCenter(s,visibilities,times)
     MapMats.inverseCovarianceWeightVisibilities(s,visibilities)
 
@@ -63,8 +64,9 @@ def Mapmaker(freq = 150, configFile = "configuration.txt", overridePSFExtension 
             
     #Renormalize maps and PSFs and save results
     Dmatrix = np.diag(np.diag(PSF[:,coords.mapIndexLocationsInExtendedIndexList])**(-1))
-    #Dmatrix = np.diag(np.ones(len(coords.mapIndices)))
-    #print "warning: Dmatrix set to Identity"
+ #   Dmatrix = np.diag(np.ones(len(coords.mapIndices)))
+ #   print "warning: Dmatrix set to Identity"
+    #Note to self: I have to decide if this is the most logical PSF. Perhaps I want the correct pixel of the "other side" to always peak at 1...
     PSF = np.dot(Dmatrix,PSF)
     coaddedMap = np.dot(Dmatrix,coaddedMap)
     mapNoiseCovariance = np.dot(PSF[:,coords.mapIndexLocationsInExtendedIndexList],np.transpose(Dmatrix))

Source/MatricesForMapmaking.py
@@ -32,7 +32,7 @@ def calculateNInv(s, snapshot):
 def calculateKAtranspose(s,snapshot,coords,PBs):
     """This function computes K_PSF * A^t, which maps baselines at the given snapshot central index to the extended facet.
     It is worth nothing that the Fourier convention is based on A having e^ib.k, so A^t has e^-ib.k = e^i|k|b.theta_hat, as we see here."""
-    realSpaceDiagonalPart = np.ones(coords.nExtendedPixels) * 4*np.pi / 12.0 / coords.NSIDE**2 * s.convertJyToKFactor
+    realSpaceDiagonalPart = np.ones(coords.nExtendedPixels) * 4*np.pi / 12.0 / coords.NSIDE**2 / s.convertJyToKFactor
     extendedAlts, extendedAzs = Geometry.convertEquatorialToHorizontal(s, coords.pixelRAs[coords.extendedIndices], coords.pixelDecs[coords.extendedIndices],snapshot.centralLST)
     extendedCartVecs = Geometry.convertAltAzToCartesian(extendedAlts, extendedAzs)
     realSpaceDiagonalPart *= hp.get_interp_val(PBs.beamSquared("X","x",s.pointings[snapshot.centralLSTIndex]), np.pi/2-extendedAlts, extendedAzs)    