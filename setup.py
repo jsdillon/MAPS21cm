fffe5ba956a881cc00bd2797a07d4055ddf29bc8
Josh Dillon <jsdiillon@mit.edu>
2015-11-01 14:24:43 -0800

    updates to gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 import glob
-from setuptools import setup
+from setuptools import setup, Command
+import os
 
-__version__ = '0.1'
+__version__ = '0.0.1'
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.egg-info')
 
 setup_args = {
     'name': 'maps21cm',
@@ -10,9 +21,11 @@ setup_args = {
     'author_email': 'jsdillon@berkeley.edu',
     'package_dir': {'maps21cm':'Source'},
     'packages': ['maps21cm'],
-    'scripts': glob.glob('Scripts/*'),
-    'version': __version__
+#    'scripts': glob.glob('Scripts/*'),
+    'version': __version__,
+    'cmdclass':{'clean': CleanCommand}
 }
+
 
 if __name__== '__main__':
     apply(setup, (), setup_args)
